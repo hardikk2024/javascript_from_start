bb24fef7afa03a0606916ddaaea008f29d5c225b
hardik verma <162896212+hardikk2024@users.noreply.github.com>
2025-11-16 02:20:21 +0530

    Add files via upload

---
+++ javascript/day1/ques.js
@@ -0,0 +1,15 @@
+// WHAT IS THE VALUE OF AGE AFTER THIS CODE RUNS 
+let age =23;
+age + 2 ;
+console.log(age);
+
+// what is the value of avg after this code runs?
+let hindi = 80;
+let eng = 90;
+let math = 100;
+let avg = (hindi + eng + math) / 3;
+
+
+
+let name = " hardik";
+console.log(name);

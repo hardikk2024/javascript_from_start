ad862d54ca18665bb3e608b6d792314be8468130
hardik verma <162896212+hardikk2024@users.noreply.github.com>
2025-12-03 00:14:50 +0530

    Add files via upload

---
+++ day2/index.js
@@ -0,0 +1,43 @@
+ /* TEMPLATE LITERALS => - ${}
+ a = 23
+ b = 32
+ let output  = `the total price is : ${a + b} rupees.`;
+ console.log(output)
+
+
+
+// example
+
+//let a = 5;
+//let b = 10;
+//console.log(`Your pay is ${a + b} rupees`);
+
+
+/*OPERATORS 
+
+-> ARITHMETIC  = - + * %
+-> UNANY  = pre ++a = phele increment karo then use karo , post increment a++ =  phele use karo then fir value increase karo
+-> ASSIGNMENT = ( = also know as sort hand notation )
+-> COMPARISON  = { ans comes in true and false  ex: <= >= == === =! }
+-> termery operator   
+-> logical ( && , ! , ||)
+
+*/
+
+// ter 
+let a = 12;
+let opt = (a > 18) ? 'I CAN ': 'CHUP MADERCHOD'
+console.log(opt)
+
+
+// logical 
+
+let ans = ( true  && true &&  true )  // if all the condition is ture then it print ture else false 
+let ans1 = ( false  || false ||  false )  // if any one is ture then everything true 
+let ans2 = !(true)  // print oppisite 
+console.log(ans2)
+
+
+// bitwise important hai 
+
+

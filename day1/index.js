74de2b10917aa903fd880803488ed845b4ada41a
hardik verma <162896212+hardikk2024@users.noreply.github.com>
2025-12-03 00:16:48 +0530

    Add files via upload

---
+++ day1/index.js
@@ -0,0 +1,83 @@
+
+
+// WHAT IS A VARIABLE ?
+// A variable is a simply the name of a storage location
+// ex = let namee = "hardik";
+
+
+// DATA TYPE IN JS
+// PRIMITIVE TYPES = NUMBER ,BOOLEAN , STRING ,UNDEFINED , NULL , BIGINT , SYMBOL ;
+
+
+// NaN in js 
+//-> the nan global property is a value representing the  not a number 
+
+// OPERATOR PRECENDENCE
+//-> THIS IS THE GENERAL ORDER OF SOLVING AN EXPEESSION EG= (),**,*,|,%,-,+;
+
+
+// LET KEYWORD - SYNTAX OF DECARING VARIABLE 
+// EG= LET AGE = 23;
+
+
+// CONST KEYWORD = VALUES OF CONSTANTS CAN'T BE CHANGED WITH RE-ASSIGNMENET & THEY CAN'T BE RE-DECLARED
+// EG= CONST YEAR = 2025;
+
+// VAR KEYWORD 
+// OLD SYNTAX OF WRITING CODE
+
+//  ASsignment operation 
+// eg = age=age+1;
+        // age+=1;
+
+
+// unary operators 
+// >unary operators also work on a single operand and are used to perform simple operations like negation, increment, decrement, and logical NOT
+// example 
+// let a = 5;
+
+// console.log(+a);      // 5
+// console.log(-a);      // -5
+// console.log(++a);     // 6 (pre-increment)
+// console.log(a++);     // 6 (post-increment), then a becomes 7
+// console.log(--a);     // 6 (pre-decrement)
+// console.log(a--);     // 6 (post-decrement), then a becomes 5
+
+// console.log(!true);   // false
+// console.log(typeof a); // "number"
+
+
+
+// STRING IN JS
+// STRING ARE TEXT OR SEQUENCE OF CHARACTERS
+
+// let names = "hardik";
+// let role = 'ku';
+
+// null and undefined in js  
+// undefined  =  a variable that is not been assigned a value is of type undefined 
+
+// null = the null value represent the intentional absence of any object value 
+
+// console.log("hello")
+
+// a=12
+// b=12
+// console.log(a+b)
+
+
+// string indices
+// this is used to check the index of any sting for example 
+
+let namee = "hardik"
+console.log(namee[4])
+// here you will see in output it will give you {i} because the index value of 4 is i 
+
+// to know of length of any string 
+console.log(namee.length)
+
+// CONCATENATION 
+// adding  multiple string exa,ple 
+let a = "apple" ;
+let b = "baba" ;
+console.log( a + b)
